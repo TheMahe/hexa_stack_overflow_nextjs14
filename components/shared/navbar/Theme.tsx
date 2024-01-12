9f39ea639ae13c342f2cc29f2924fdfd33024fe6
Madevelopment <mahir.delic.07@gmail.com>
2024-01-12 21:11:17 +0100

    implemented theme change

--- components/shared/navbar/Theme.tsx
+++ components/shared/navbar/Theme.tsx
@@ -18,7 +18,7 @@ const Theme = () => {
     <Menubar className="relative border-none bg-transparent shadow-none">
       <MenubarMenu>
         <MenubarTrigger className="focus:bg-light-900 data-[state=open]:bg-light-900 dark:focus:bg-dark-200 dark:data-[state=open]:bg-dark-200">
-          {/* {mode === "light" ? (
+          {mode === "light" ? (
             <Image
               src="/assets/icons/sun.svg"
               width={20}
@@ -34,11 +34,23 @@ const Theme = () => {
               alt="sun"
               className="active-theme"
             />
-          )} */}
+          )}
         </MenubarTrigger>
         <MenubarContent className="absolute right-[-3rem] mt-3 min-w-[120px] rounded border py-2 dark:border-dark-400 dark:bg-dark-300 ">
           {themes.map((item) => (
-            <MenubarItem key={item.value} onClick={() => {}}>
+            <MenubarItem
+              key={item.value}
+              className="flex items-center gap-4 px-2.5 py-2 dark:focus:bg-dark-400"
+              onClick={() => {
+                setMode(item.value);
+
+                if (item.value !== "system") {
+                  localStorage.theme = item.value;
+                } else {
+                  localStorage.removeItem("theme");
+                }
+              }}
+            >
               <Image
                 src={item.icon}
                 alt={item.value}
@@ -46,6 +58,15 @@ const Theme = () => {
                 height={16}
                 className={`${mode === item.value ? "active-theme" : ""}`}
               />
+              <p
+                className={`body-semibold text-light-500 ${
+                  mode === item.value
+                    ? "text-primary-500"
+                    : "text-dark100_light900"
+                }`}
+              >
+                {item.label}
+              </p>
             </MenubarItem>
           ))}
         </MenubarContent>
